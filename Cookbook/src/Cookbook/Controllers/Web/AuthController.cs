b49135bb5279d217f467947624adb2656e3f9d3d
Pany Koutsios <pkoutsios@hotmail.com>
2016-10-12 23:27:20 +0100

    Email confirmation

--- Cookbook/src/Cookbook/Controllers/Web/AuthController.cs
+++ Cookbook/src/Cookbook/Controllers/Web/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using Cookbook.Models;
 using Cookbook.ViewModels;
+using Cookbook.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -14,15 +15,18 @@ namespace Cookbook.Controllers.Web
         private SignInManager<CookbookUser> _signInManager;
         private UserManager<CookbookUser> _userManager;
         private ILogger<AuthController> _log;
+        private IEmailSender _emailSender;
 
         public AuthController(
             SignInManager<CookbookUser> signInManager, 
             UserManager<CookbookUser> userManager, 
-            ILogger<AuthController> logger)
+            ILogger<AuthController> logger,
+            IEmailSender emailSender)
         {
             _signInManager = signInManager;
             _userManager = userManager;
             _log = logger;
+            _emailSender = emailSender;
         }
 
         public IActionResult Register()
@@ -45,12 +49,19 @@ namespace Cookbook.Controllers.Web
                 IdentityResult result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var callbackUrl = Url.Action("ConfirmEmail", "Auth", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
+                    await _emailSender.SendEmailAsync(
+                        user.Email,
+                        "Please confirm your email address", 
+                        $"Hi,\nThank you for registering for a Cookbook account\nPlease confirm your email\n<a href='{callbackUrl}'>Confirm Email</a>");
+                    //await _signInManager.SignInAsync(user, isPersistent: true);
+
                     _log.LogInformation(3, "User created a new account with password.");
                     return RedirectToAction(nameof(RecipeController.Index), "Home");
                 }
-                ModelState.AddModelError("", "Registration failed");
-                return BadRequest(result.Errors);
+
+                AddErrors(result);
             }
 
             return View(model);
@@ -72,8 +83,17 @@ namespace Cookbook.Controllers.Web
         {
             if (ModelState.IsValid)
             {
-                var signinResult = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, true, false);
+                var user = await _userManager.FindByNameAsync(model.UserName);
+                if (user != null)
+                {
+                    if (!await _userManager.IsEmailConfirmedAsync(user))
+                    {
+                        ModelState.AddModelError(string.Empty, "You must have a confirmed email to log in.");
+                        return View(model);
+                    }
+                }
 
+                var signinResult = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, true, false);
                 if (signinResult.Succeeded)
                 {
                     return RedirectToAction(nameof(RecipeController.Index), "Recipe");
@@ -94,5 +114,34 @@ namespace Cookbook.Controllers.Web
             return RedirectToAction(nameof(RecipeController.Index), "Home");
         }
 
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> ConfirmEmail(string userId, string code)
+        {
+            if (userId == null || code == null)
+            {
+                return View("Error");   // TODO redirect
+            }
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return View("Error");   // TODO redirect
+            }
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            return View(result.Succeeded ? "ConfirmEmail" : "Error");
+        }
+
+
+        #region Helpers
+
+        private void AddErrors (IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        #endregion
     }
 }
---
+++ Cookbook/src/Cookbook/Services/MessageServices.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Options;
+using SendGrid;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Cookbook.Services
+{
+    public class AuthMessageSender : IEmailSender
+    {
+        public AuthMessageSenderOptions Options { get; }
+
+        public AuthMessageSender(IOptions<AuthMessageSenderOptions> options)
+        {
+            Options = options.Value;
+        }
+
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            // Plug in your email service here to send an email.
+            var myMessage = new SendGridMessage();
+            myMessage.AddTo(email);
+            myMessage.From = new MailAddress("noreply@cookbook.com", "Cookbook Account");
+            myMessage.Subject = subject;
+            myMessage.Text = message;
+            myMessage.Html = message;
+
+            // Create a Web transport for sending email.
+            var transportWeb = new Web(Options.SendGridKey);
+            return transportWeb.DeliverAsync(myMessage);
+        }
+    }
+}
