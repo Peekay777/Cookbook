5102c21437dace9e9615071f3bbed204bc21037b
Pany <pkoutsios@hotmail.com>
2016-10-26 10:28:51 +0100

    Defaults added to message sender properties

--- Cookbook/src/Cookbook/Services/EmailSender/AuthMessageSenderOptions.cs
+++ Cookbook/src/Cookbook/Services/EmailSender/AuthMessageSenderOptions.cs
@@ -3,7 +3,7 @@
     public class AuthMessageSenderOptions
     {
         public string SendGridKey { get; set; }
-        public string Address { get; set; }
-        public string DisplayName { get; set; }
+        public string Address { get; set; } = "noreply@cookbook.com";
+        public string DisplayName { get; set; } = "Cookbook Account";
     }
 }
